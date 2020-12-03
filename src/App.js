0ab7d68f48f77277fad74b1682035f867528b706
dantedxwolf <teacher.martin@gmail.com>
2020-12-03 11:58:16 +0600

    intial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          now, the container is fully working hello yo
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+       annie time lll
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
